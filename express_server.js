bd0e940083dd0b174dbc1bbcd9bf2d094bbdeecb
Roderick Nuque <enuke4@gmail.com>
2021-12-07 06:25:20 +0000

    add a GET route to show the form & add a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -27,10 +30,21 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+//Add a GET route to show the form
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 //Render information about the a single URL
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.longURL] };
   res.render('urls_show', templateVars);
+});
+
+//Add a POST route to receive the form submission
+app.post('urls', (req, res) => {
+  console.log(req.body); //Log the POST request body to the console
+  res.send('Ok'); //Respond with 'ok'
 });
 
 app.listen(PORT, () => {
