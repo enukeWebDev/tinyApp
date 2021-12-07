c1f28f14eb637cdd213564d6b430a6f10bdff511
Roderick Nuque <enuke4@gmail.com>
2021-12-07 07:59:30 +0000

    finished the part 2 of the project

--- express_server.js
+++ express_server.js
@@ -4,7 +4,6 @@ const PORT = 8080; //default port
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -20,10 +19,6 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
 //Add a route for /urls
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -35,18 +30,39 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+//Add a POST route to receive the form submission
+app.post('/urls', (req, res) => {
+  console.log(req.body); //Log the POST request body to the console
+  res.send('Ok'); //Respond with 'ok'
+});
+
 //Render information about the a single URL
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.longURL] };
+  let shortURL = req.params.shortURL; //Assign to a variable for easy access later - keys
+  let longURL = urlDatabase[shortURL]; //Assign to a variable for easy access later - values
+  const templateVars = { shortURL, longURL };
+  // const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
 
-//Add a POST route to receive the form submission
-app.post('urls', (req, res) => {
-  console.log(req.body); //Log the POST request body to the console
-  res.send('Ok'); //Respond with 'ok'
+//Redirect any request to it's long URL
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+//Function to generate & returns a string of 6 random alphanumeric characters
+//using base 36 toString to look up from numbers (0-9) & letters (A-Z)
+//substr will extract characters to form the 6 random characters
+function generateRandomString() {
+  let randomStr = Math.random().toString(36).substr(0, 5);
+  return randomStr;
+}
