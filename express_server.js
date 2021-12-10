2d36e44520185f41835bf1248b68a0d5ac3ca97f
Roderick Nuque <enuke4@gmail.com>
2021-12-10 03:42:23 +0000

    update

--- express_server.js
+++ express_server.js
@@ -1,12 +1,20 @@
+const { response } = require("express");//** 
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcryptjs')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(cookieParser());
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['user_id']
+}));
 
 //Sample Data**
 const users = {
@@ -23,9 +31,21 @@ const users = {
   }
 };
 
+/*
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
+};
+*/
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
 };
 
 app.get('/', (req, res) => {
@@ -36,12 +56,58 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/login', (req, res) => {
+  const templateVars = {
+    urls: urlDatabase,
+    users: users,
+    //username: null, //no user**
+    user: lookUpLoggedInUser(req)
+  };
+  res.render('urls_login', templateVars);
+});
+
+app.post('/login', (req, res) => {
+  //Access the email & password 
+  //Check if user exists or not
+  //Check if password matches or not
+  //Throw error status if not match
+  //Assign email cookie using the user email
+  //Redirect to /urls
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const bcryptPassword = bcrypt.hashSync(password, 8);
+  const syncPassword = bcrypt.compareSync(password, bcryptPassword);
+  const user_id = lookUpUserByEmail(email);
+
+
+  if (email === '' || password === '') {
+    res.status(400).send('Email & Password Cannot Be Blank - Try Again!');
+  }
+
+  if (!user_id) {
+    res.status(403).send('Email Does Not Exist - Try Again!');
+  }
+
+  if (!syncPassword) {
+    res.status(403).send('Password Does Not Much - Try Again!');
+  }
+
+  if (user_id && syncPassword) {
+    req.session.user_id = user_id;
+  }
+  res.redirect('/urls');
+});
+
+
 //Add a route for /urls
 app.get('/urls', (req, res) => {
   //const templateVars = { urls: urlDatabase, username: req.cookies.username }; //Passing the username
   const templateVars = {
+    compressed: compressDatabase(req.session.user_id),
     urls: urlDatabase,
     users: users,
+    accountInfo: req.session.user_id,
     user: lookUpLoggedInUser(req)
   };
   res.render('urls_index', templateVars);
@@ -50,12 +116,16 @@ app.get('/urls', (req, res) => {
 //Add a GET route to show the form
 //** 
 app.get('/urls/new', (req, res) => {
-  const templateVars = {
-    urls: urlDatabase,
-    users: users,
-    user: lookUpLoggedInUser(req)
-  }; //Passing the username
-  res.render('urls_new', templateVars);
+  if (!lookUpLoggedInUser(req)) {
+    res.redirect('/login');
+  } else {
+    const templateVars = {
+      urls: urlDatabase,
+      users: users,
+      user: lookUpLoggedInUser(req)
+    };
+    res.render('urls_new', templateVars);
+  }
 });
 
 //Add a POST route to receive the form submission
@@ -65,7 +135,7 @@ app.post('/urls', (req, res) => {
   let randomAlphaNumeric = generateRandomString(); //Tues - generate the random alpha-numeric code
   urlDatabase[randomAlphaNumeric] = {
     longURL: req.body.longURL,
-    userID: req.cookies.user_id
+    userID: req.session.user_id
   };
   //console.log(`req.body);
   res.redirect(`/urls/${randomAlphaNumeric}`);
@@ -74,13 +144,28 @@ app.post('/urls', (req, res) => {
 //Render information about the a single URL
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL; //Assign to a variable for easy access later - keys
-  const longURL = urlDatabase[shortURL]; //Assign to a variable for easy access later - values
-  const templateVars = { shortURL, longURL, username: req.cookies.username }; //Passing the username
+  const longURL = urlDatabase[shortURL].longURL; //Assign to a variable for easy access later - values
+  const templateVars = {
+    shortURL,
+    longURL,
+    users: users,
+    user: lookUpLoggedInUser(req)
+  };
   // const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  if (urlDatabase[shortURL].userID !== req.session.user_id) {
+    res.status(400).send('Warning - You have NO Authorization!');
+  }
   res.render('urls_show', templateVars);
 });
 
-//
+app.post('/urls/:shortURL', (req, res) => {
+  //const postShortURL = ;
+
+  urlDatabase[req.params.shortURL][longURL] = req.body.newURL;
+  res.redirect('/urls/');
+})
+
+/*
 app.post('/urls/:id', (req, res) => {
   const shortURL = req.params.id;
   const newURL = req.body.newURL;
@@ -88,7 +173,7 @@ app.post('/urls/:id', (req, res) => {
   urlDatabase[shortURL] = newURL;
   res.redirect('/urls');
 });
-
+*/
 //Delete the created shortURL
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortURL = req.params.shortURL;
@@ -98,8 +183,8 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 
 //Redirect any request to it's long URL
 app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  console.log(req.params.shortURL, longURL); //Tues - add
+  const longURL = urlDatabase[req.params.shortURL]['longURL'];
+  //console.log(req.params.shortURL, longURL); //Tues - add
   res.redirect(longURL);
 });
 
@@ -119,40 +204,10 @@ app.post('/login', (req, res) => {
 });
 */
 //** 
-app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
 
-  if (!email || !password) {
-    return res.status(400).send('Email & Password Cannot Be Blank - Try Again!');
-  }
-  const user_id = lookUpUserByEmail(email);
-
-  if (!user_id) {
-    return res.status(403).send('Email Does Not Exist - Try Again!');
-  }
-
-  if (users[user_id].password !== password) {
-    return res.status(403).send('Password Does Not Much - Try Again!');
-  }
-
-  if (user_id && users[user_id].password === password) {
-    res.cookie('user_id', user_id);
-  }
-  res.redirect('/urls');
-});
 
 //** 
 //Login endpoint
-app.get('/login', (req, res) => {
-  const templateVars = {
-    urls: urlDatabase,
-    users: users,
-    //username: null, //no user**
-    user: lookUpLoggedInUser(req)
-  };
-  res.render('urls_login', templateVars);
-});
 
 //The Logout Route
 /*
@@ -163,7 +218,7 @@ app.post('/logout', (req, res) => {
 */
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect('/urls');
 });
 
@@ -183,22 +238,19 @@ app.post('/register', (req, res) => {
   const id = generateRandomString();
   const email = req.body.email;
   const password = req.body.password;
+  const bcryptPassword = bcrypt.hashSync(password, 8);
 
   if (email === '' || password === '') {
     res.status(400).send('Email & Password Cannot Be Blank - Try Again!');
   }
-
   else if (!lookUpLoggedInUser(req)) {
-    users[id] = {
-      id, email, password
-    };
-    res.cookie('user', id);
-    res.redirect('urls');
+    users[id] = { id, email, password: bcryptPassword };
   }
-
-  else {
+  else if (lookUpUserByEmail(email)) {
     res.status(400).send('Email Already in the System');
   }
+  req.session.user_id = id;
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
@@ -215,7 +267,7 @@ function generateRandomString() {
 
 //Function to look up for logged in user**
 function lookUpLoggedInUser(req) {
-  const user_id = req.cookies.user_id;
+  const user_id = req.session.user_id;
   if (users[user_id]) {
     return users[user_id];
   }
@@ -231,4 +283,14 @@ function lookUpUserByEmail(email) {
     }
   }
   return null; //email does not exist
+};
+
+const compressDatabase = (accountInfo) => {
+  let myDatabase = {};
+  for (const info in urlDatabase) {
+    if (urlDatabase[info].user_id === accountInfo) {
+      myDatabase[info] = urlDatabase[info];
+    }
+  }
+  return myDatabase;
 };
